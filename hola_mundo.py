e81a1cfc24dc31d42cf0aaeace81b9161515e9ad
francarrasc0 <fran.carrascolozano@gmail.com>
2023-10-02 23:48:10 -0300

    Hola mundo

---
+++ hola_mundo.py
@@ -0,0 +1,19 @@
+#1.-
+print ("Hola, mundo")
+
+#2.-
+name = "Francisca"
+print ("¡Hola", name, "!")
+print("¡Hola" + name + "!")
+
+#3.-
+num= 27
+print("Hola", num, "!")
+print("Hola " + str(num) + "!")
+
+#4.-
+food1= "pizza"
+food2= "hamburguesa"
+print("Amo comer {} y {}.".format(food1, food2))
+print(f"Amo comer {food1} y {food2}.")
+print("Amo comer %s y %s." % (food1, food2))
